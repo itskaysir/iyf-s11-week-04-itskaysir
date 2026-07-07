110d684876368d4d9b98f2788af47b77b44121a7
itskaysir <antonyjuma300@gmail.com>
2026-07-07 17:01:31 +0300

    Added numberOperations.js and stringOperations.js files

---
+++ stringOperations.js
@@ -0,0 +1,10 @@
+let firstName = "Kaysir";
+let lastName = "Antony";
+let fullName = firstName + " " + lastName;
+
+let greeting = `Hello, my name is ${fullName}.`;
+let message = `Your name has ${fullName.length} characters.`;
+
+
+console.log(greeting);
+console.log(message);
