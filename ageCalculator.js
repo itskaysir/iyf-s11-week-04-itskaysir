6be780707d050871ffefba25b365d2540897b177
itskaysir <antonyjuma300@gmail.com>
2026-07-07 17:27:52 +0300

    Added age calculator for my age in days and hours

---
+++ ageCalculator.js
@@ -0,0 +1,9 @@
+let age = 2026 - 2002
+let ageInDays = age * 365
+console.log("My age in days is: " + ageInDays + " days")
+
+let ageInHours = ageInDays * 24
+console.log("My age in hours is: " + ageInHours + " hours")
+
+let yearWhenIWillBe100 = 2002 + 100
+console.log("I will be 100 years old in the year: " + yearWhenIWillBe100) 
