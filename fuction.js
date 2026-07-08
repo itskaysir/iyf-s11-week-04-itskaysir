06e3b181b5d7d6d83dde7fe2ed1b3511df9dcc3e
itskaysir <antonyjuma300@gmail.com>
2026-07-08 15:34:22 +0300

    Added function.js practice file

---
+++ fuction.js
@@ -0,0 +1,30 @@
+const calculateArea = (length, width) => {
+    return length * width;
+}
+
+calculateArea(5, 10); // Returns 50
+
+const celciusToFahrenheit = (celsius) => {
+    return (celsius * 9/5) + 32;
+}
+
+celciusToFahrenheit(25); // Returns 77
+
+const isEven = (number) => {
+    return number % 2 === 0;
+}
+
+isEven(4); // Returns true
+
+const getInitials = (fullName) => {
+    const [firstName, lastName] = fullName.split(' ');
+    return `${firstName[0]}${lastName[0]}`; 
+};
+
+getInitials('Kaysir Antony'); // Returns 'KA'
+
+const reverseString = (str) => {
+    return str.split('').reverse().join('');
+}
+
+reverseString('Hello'); // Returns 'olleH'
